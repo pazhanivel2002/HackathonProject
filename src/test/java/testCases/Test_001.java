9cf0b049471bd969dce3ba4b55dae4b578f31cff
PAZHANIVEL S K <141727503+pazhanivel2002@users.noreply.github.com>
2024-03-11 12:03:29 +0530

    Update Test_001.java

--- src/test/java/testCases/Test_001.java
+++ src/test/java/testCases/Test_001.java
@@ -76,7 +76,7 @@ public class Test_001 extends BaseClass {
 	public void verifyValidLoanWebPage() {
 		hl.navLoanCalculator();
 		logger.info("Validation of web page title started");
-		Assert.assertEquals(driver.getTitle(),"Loan Calculator — Calculate EMI, Affordability, Tenure & Interest Rate");
+		Assert.assertEquals(driver.getTitle(),"Loan Calculator � Calculate EMI, Affordability, Tenure & Interest Rate");
 		logger.info("Validation of WebPage Title is done");
 		captureScreen("img_titleValidationLoanCalculator");
 	}
